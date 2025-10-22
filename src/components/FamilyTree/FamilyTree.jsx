c31b419c8b5f0f824d5ee3b1a7c7845d67a1d577
Tamanna <tahminadox@gmail.com>
2025-10-22 11:21:27 +0600

    Introduction to Context API and simple context

--- src/components/FamilyTree/FamilyTree.jsx
+++ src/components/FamilyTree/FamilyTree.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import Grandpa from './Grandpa';
 import './FamilyTree.css';
+
+export const AssetContext = createContext('');
 
 const FamilyTree = () => {
 
     const asset = 'diamond';
+    const newAsset = 'gold';
 
     return (
         <div className='family-tree'>
             <h1>Family Tree</h1>
-            <Grandpa asset={asset}/>
+            <AssetContext.Provider value={newAsset}>
+                <Grandpa asset={asset}/>
+            </AssetContext.Provider>
+            
         </div>
     );
 };
--- src/components/FamilyTree/Special.jsx
+++ src/components/FamilyTree/Special.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AssetContext } from './FamilyTree';
 
 const Special = ({name, asset}) => {
+
+    const newAsset = useContext(AssetContext);
+    console.log('new asset: ', newAsset);
+
     return (
         <div>
             <h3>Special: {name}</h3>
             <p>Asset: {asset}</p>
+            <p>New Asset: {newAsset}</p>
         </div>
     );
 };
